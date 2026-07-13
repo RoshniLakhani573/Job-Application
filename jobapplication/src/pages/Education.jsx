99501bc84a467ba22b0a4b6ee4bf9fa1e36211b6
RoshniLakhani573 <roshinilakhani@gmail.com>
2026-07-13 09:32:37 +0000

    added Education page

--- jobapplication/src/pages/Education.jsx
+++ jobapplication/src/pages/Education.jsx
@@ -1,47 +1,105 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Education =()=>{
-    const [data1,setData1] = useState({
+    const navigate = useNavigate()
+    const [data,setData] = useState({
         intermediate :"",
-        pasIntermediateDate:"",
-        collegeName :"",
-        pasCollegeDate:""
+        passIntermediateDate:"",
+        graduation :"",
+        passGraduationDate:""
     })
-    const handleChange=()=>{
-        return(<></>)
+    const handleChange=(e)=>{
+        return(
+            setData({
+                ...data,
+                [e.target.name]:e.target.value
+            })
+        )
     }
-    const handleSubmit=()=>{}
+        const handleSubmit=()=>{
+        e.preventDefault();
+
+        const newError = {
+            intermediate:"",
+            passIntermediateDate:"",
+            graduation:"",
+            passGraduationDate:""
+        }
+
+        const CollegeNameRegex = /^[A-Za-z0-9\s.,&()-]+$/;
+        const PassOutDate = /^\d{4}-\d{2}-\d{2}$/;
+
+        // intermediate
+        if (!data.intermediate.trim()){
+            newError.intermediate = "enter college name "
+        }else if (!CollegeNameRegex.test(data.intermediate)){
+            newError.intermediate = "enter valid College Name"
+        }
+        
+        // intermediate
+        if (!data.passIntermediateDate.trim()){
+            newError.passIntermediateDate = "enter your passout Date "
+        }else if (!PassOutDate.test(data.passIntermediateDate)){
+            newError.passIntermediateDate = "enter valid Passout Date"
+        }
+        
+        // graduation
+        if (!data.graduation.trim()){
+            newError.graduation = "enter college name "
+        }else if (!CollegeNameRegex.test(data.intermediate)){
+            newError.intermediate = "enter valid Graduation College Name"
+        }
+
+        // passout Date
+        if (!data.passGraduationDate.trim()){
+            newError.passGraduationDate = "enter college name "
+        }else if (!PassOutDate.test(data.passGraduationDate)){
+            newError.passGraduationDate = "enter valid passout Graduation Date"
+        }
+        
+        if (!data.intermediate||!data.passIntermediateDate||data.graduation||data.passGraduationDate){
+            return;
+        }
+        localStorage.setitem("data",JSON.stringify(data))
+        console.log(data)
+        navigate("/workexperience")
+        
+    }
+
     
     return(
         <div>
-            <form>
+            <form on onSubmit={handleSubmit}>
                 <label>College Name(Intermediate)</label>
                 <input
                 type = "text"
                 name = "intermediate"
-                value={data1.intermediate}
+                value={data.intermediate}
                 onChange={handleChange} />
+                <br/>
                 <label>Passing Date</label>
                 <input
                 type = "text"
-                name="pasIntermediateDate"
-                value={data1.pasIntermediateDate}
+                name="passIntermediateDate"
+                value={data.passIntermediateDate}
                 onChange={handleChange}/>
-
+                <br/>
                  <label>College Name</label>
                 <input
                 type = "text"
-                name = "collegeName"
-                value={data1.collegeName}
+                name = "graduation"
+                value={data.graduation}
                 onChange={handleChange} />
+                <br/>
                 <label>Passing Date</label>
                 <input
                 type = "text"
-                name="pasCollegeDate"
-                value={data1.pasCollegeDate}
+                name="passGraduationDate"
+                value={data.passGraduationDate}
                 onChange={handleChange}/>
    
-                <button>Next</button>
+                <button type="submit">Next</button>
             </form>
         </div>
     )
--- jobapplication/src/pages/WorkExperience.jsx
+++ jobapplication/src/pages/WorkExperience.jsx
@@ -1,33 +1,3 @@
-import { useState } from "react";
-
-const WorkExperience =()=>{
-    const [data,setData] = useState({
-        companyName:"",
-        experience:"",
-        joiningDate: "",
-        lastDate: ""
-    })
-    return(
-        <div>
-            <form>
-                <label>Company Name(Intermediate)</label>
-                <input
-                type = "text"
-                name = "companyName"
-                value= {companyName}
-                onChange={handleChange}
-                required />
-                <label>Year Of Experience</label>
-                <input
-                type="text"
-                name = "experience"
-                value={experience}
-                onChange={handleChange}/>
-
-                <label>Date of joining</label>
-                <label>Last Date</label>
-            </form>
-        </div>
-    )
+const WorkExperience = ()=>{
+    
 }
-export default WorkExperience;
