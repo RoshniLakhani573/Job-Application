ffd02437d9c23b99895c7d08b67e90d020a3dbe2
RoshniLakhani573 <roshinilakhani@gmail.com>
2026-07-13 09:56:29 +0000

    added WorkExperience page

--- jobapplication/src/pages/WorkExperience.jsx
+++ jobapplication/src/pages/WorkExperience.jsx
@@ -1,3 +1,48 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const WorkExperience = ()=>{
-    
+    const navigate = useNavigate();
+    const [data,setData]= useState({
+        companyName:"",
+        yearsOfExperience:"",
+        dateOfJoining:""
+    })
+
+    const [error,setError] = useState({
+        companyName:"",
+        yearsOfExperience:"",
+        dateOfJoining:""       
+    })
+    const handleChange =(e)=>{
+        setData({
+            ...data,
+            [e.target.name]:e.target.value
+        })
+
+        const handleSubmit = ()=>{
+            e.preventDefault();
+            const CompanyNameRegrex= /^[A-Za-z0-9\s.,&()-]+$/;
+            const DateOfJoiningRegrex=/^\d{4}-\d{2}-\d{2}$/;
+            const ExperienceRegex = /^(?:[0-9]|[1-4][0-9]|50)(?:\.[0-9])?$/;
+            
+            if(!data.companyName.trim){
+                
+            }
+
+        }
+    }
+    return(<div>
+        <form>
+            <label>Current Company Name</label>
+            <input 
+            type="text"
+            name=""/>
+            <label>Year of Experence </label>
+            <input/> 
+            <label>Date of Joining</label>
+            <input/>
+        </form>
+        </div>)
+
 }
